34f74edf6607d9b16480ce3f3570600953c66342
goyo-sagamino <shousagami@gamil.com>
2018-12-25 15:56:46 +0900

    add change

--- FREE_UI/script/script.js
+++ FREE_UI/script/script.js
@@ -2,19 +2,19 @@ window.onload = function(){
 
 	let pathname = location.pathname;
 	
-    if (pathname === "/D:/Desktop/meister/FREE_UI/HTML/index.html") {
+    if (pathname === "/C:/Users/naito%20sho/Desktop/meister/FREE_UI/HTML/index.html") {
         let obj = document.getElementById("top");
         obj.style.backgroundColor = "rgba(0,128,128,0.3)";
 
-    } else if (pathname === "/D:/Desktop/meister/FREE_UI/HTML/form.html") {
+    } else if (pathname === "/C:/Users/naito%20sho/Desktop/meister/FREE_UI/HTML/form.html") {
         let obj = document.getElementById("form");
         obj.style.backgroundColor = "rgba(0,128,128,0.3)";
 
-    } else if (pathname === "/D:/Desktop/meister/FREE_UI/HTML/report.html") {
+    } else if (pathname === "/C:/Users/naito%20sho/Desktop/meister/FREE_UI/HTML/report.html") {
         let obj = document.getElementById("report");
         obj.style.backgroundColor = "rgba(0,128,128,0.3)";
 
-    } else if (pathname === "/D:/Desktop/meister/FREE_UI/HTML/help.html") {
+    } else if (pathname === "/C:/Users/naito%20sho/Desktop/meister/FREE_UI/HTML/help.html") {
         let obj = document.getElementById("help");
         obj.style.backgroundColor = "rgba(0,128,128,0.3)";
 
